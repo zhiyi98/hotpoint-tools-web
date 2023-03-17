67353cacdfd022e9a7465d4953027d7efbb17ce1
zhiyi98 <yuanzhiyi98@gmail.com>
2023-03-17 23:04:29 +0800

    feat: Update Home Component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,19 +14,46 @@
     <div class="flex items-center justify-center space-x-6 mr-6">
       <div><img src="https://file.ipadown.com/tophub/assets/images/hi.png" width="50" alt=""></div>
       <div class="text-gray-500">App</div>
-      <div class="rounded-full overflow-hidden w-8">
-        <a href="#">
-          <img src="https://lh3.googleusercontent.com/a/AGNmyxbXHav6xbZuy0L-gRUx4Z4jZp4RAnt6MN5TXve4=s96-c" alt="">
-        </a>
-      </div>
+      <!-- Profile dropdown -->
+      <Menu as="div" class="relative ml-3">
+        <div>
+          <MenuButton class="flex rounded-full bg-gray-800 text-sm">
+            <span class="sr-only">Open user menu</span>
+            <img class="h-8 w-8 rounded-full"
+                 src="https://lh3.googleusercontent.com/a/AGNmyxbXHav6xbZuy0L-gRUx4Z4jZp4RAnt6MN5TXve4=s96-c"
+                 alt=""/>
+          </MenuButton>
+        </div>
+        <transition enter-active-class="transition ease-out duration-100"
+                    enter-from-class="transform opacity-0 scale-95"
+                    enter-to-class="transform opacity-100 scale-100"
+                    leave-active-class="transition ease-in duration-75"
+                    leave-from-class="transform opacity-100 scale-100"
+                    leave-to-class="transform opacity-0 scale-95">
+          <MenuItems
+              class="absolute right-0 z-10 mt-2 w-24 text-center origin-top-right bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">个人中心</a>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">创建节点</a>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">设&emsp;&emsp;置</a>
+            </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">退&emsp;&emsp;出</a>
+            </MenuItem>
+          </MenuItems>
+        </transition>
+      </Menu>
+
     </div>
   </div>
 </template>
-<script>
+<script setup>
 import IconSearch from "../icon/IconSearch.vue";
+import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
 
-export default {
-  name: 'search-bar',
-  components: {IconSearch}
-}
+
 </script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,13 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin')
   ],
 }
